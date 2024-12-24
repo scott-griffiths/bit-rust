70582fd520cd96efdeb13b0dad82eaab808df45b
Scott Griffiths <dr.scottgriffiths@gmail.com>
2024-12-24 07:42:18 +0000

    New method to convert bytes to format that works well in Python to convert to an int.

--- src/bits.rs
+++ src/bits.rs
@@ -400,7 +400,23 @@ impl BitRust {
 
     // Just the byte data without any shifting or padding.
     pub fn to_byte_data_with_offset(&self) -> (Vec<u8>, u64) {
-        (self.data[self.start_byte()..self.end_byte()].to_vec(), self.offset)
+        (self.data[self.start_byte()..self.end_byte()].to_vec(), self.offset % 8)
+    }
+
+    // Return bytes that can easily be converted to a uint in Python
+    pub fn to_uint_byte_data(&self) -> Vec<u8> {
+        // Want the offset to make there be no padding.
+        let new_offset = (self.offset + (8 - (self.length + self.offset) % 8)) % 8;
+        debug_assert!((new_offset + self.length) % 8 == 0);
+        let mut t = if self.offset == new_offset {
+             self.data[self.start_byte()..self.end_byte()].to_vec()
+        } else {
+            self.copy_with_new_offset(new_offset).data.to_vec()
+        };
+        if new_offset != 0 {
+            t[0] &= (1 << (8 - new_offset)) - 1;
+        }
+        t
     }
 
     pub fn to_hex(&self) -> PyResult<String> {
